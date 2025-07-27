e5bb9d7fe3902ed8c64c278851400f25d4f1903d
shaon <aminul007.ais@gmail.com>
2025-07-28 00:36:30 +0600

    buttons are done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <div>
       Hello world
+      <Button variant={"primary"}>Tap tap</Button>
+      <Button variant={"optional"}>Tap tap</Button>
+      <Button variant={"default"}>Tap tap</Button>
     </div>
   );
 }
