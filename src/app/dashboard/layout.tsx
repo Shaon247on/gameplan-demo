41a5451161910a882757063baa357070b5bb9f0b
Shaon <aminul007.ais@gmail.com>
2025-08-14 02:03:32 +0600

    fix deployment issue again-5

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Navbar from "@/components/shared/Navbar";
 import Sidebar from "@/components/shared/Sidebar";
 import MainContent from "@/components/shared/MainContent";
@@ -5,24 +6,25 @@ import MobileBottomNav from "@/components/shared/MobileBottomNav";
 import StoreProvider from "@/store/StoreProvider";
 import { AuthProvider } from "@/components/auth/AuthProvider";
 import { SidebarProvider } from "@/components/shared/SidebarContext";
+import { Suspense } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-      <StoreProvider>
-        <AuthProvider>
-          <SidebarProvider>
-            <div className="flex h-screen bg-gray-50">
-              <Sidebar />
-              <MainContent>
-                <Navbar />
-                <main className="flex-1 p-2 md:p-4 lg:p-6 overflow-auto">
-                  {children}
-                </main>
-              </MainContent>
-              <MobileBottomNav />
-            </div>
-          </SidebarProvider>
-        </AuthProvider>
-      </StoreProvider>
+    <StoreProvider>
+      <AuthProvider>
+        <SidebarProvider>
+          <div className="flex h-screen bg-gray-50">
+            <Sidebar />
+            <MainContent>
+              <Navbar />
+              <main className="flex-1 p-2 md:p-4 lg:p-6 overflow-auto">
+                <Suspense>{children}</Suspense>
+              </main>
+            </MainContent>
+            <MobileBottomNav />
+          </div>
+        </SidebarProvider>
+      </AuthProvider>
+    </StoreProvider>
   );
 }
